c3b4414a2f9e5e6e018d9effe40e608668fd0214
Leisan <Leisan>
2025-03-12 19:05:39 +1100

    Added remove function

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,24 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-function createCard(cardData) {
+function createCard(cardData, removeFunction) {
     const card = cardTemplate.querySelector('.places__item').cloneNode(true);
     card.querySelector('.card__image').src = cardData.link;
     card.querySelector('.card__title').textContent = cardData.name;
+    card.querySelector('.card__delete-button').addEventListener('click', () => removeFunction(card));
 
     return card;
 }
 
 // @todo: Функция удаления карточки
 
+function removeCard(card) {
+    card.remove();
+}
+
 // @todo: Вывести карточки на страницу
 
-const createCards = initialCards.map(cardData => createCard(cardData));
+const createCards = initialCards.map(cardData => createCard(cardData, removeCard));
 
 createCards.forEach((card) => {
     document.querySelector('.places__list').append(card);
