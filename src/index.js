9fe14108d3c5b8e1b8048e74dff539c67242221a
Leisan <Leisan>
2025-04-04 23:42:46 +1100

    added webpack configuration, changed image paths for webpack, moved everything to src, added import for css and js, removed <script> and <link>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './pages/index.css'; 
+import { initialCards } from './cards.js';
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsList = document.querySelector('.places__list')
