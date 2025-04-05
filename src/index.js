7976ee9da6d1d8d5786619b90e4b2ff01aa03b2a
Leisan <Leisan>
2025-04-06 06:25:17 +1000

    Added open close logic for add_card and change_profile

--- src/index.js
+++ src/index.js
@@ -32,3 +32,53 @@ const cards = initialCards.map(cardData => createCard(cardData, removeCard));
 cards.forEach((card) => {
     cardsList.append(card);
 })
+
+
+function openPopup(popup) {
+    popup.classList.add('popup_is-opened');
+    document.addEventListener('keydown', handleEsc);
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup_is-opened');
+    document.removeEventListener('keypress', handleEsc);
+}
+
+function handleEsc(evt) {
+    if (evt.key === 'Escape') {
+        const openedPopup = document.querySelector('.popup_is-opened');
+        if (!openedPopup) return;
+        closePopup(openedPopup);
+    }
+}
+
+
+const editButton = document.querySelector('.profile__edit-button');
+const popupEdit = document.querySelector('.popup_type_edit');
+
+editButton.addEventListener('click', () => openPopup(popupEdit));
+
+const addButton = document.querySelector('.profile__add-button');
+const popupAdd = document.querySelector('.popup_type_new-card');
+
+addButton.addEventListener('click', () => openPopup(popupAdd));
+
+const closeButtons = document.querySelectorAll('.popup__close');
+
+closeButtons.forEach((closeButton) => {
+    closeButton.addEventListener('click', () => {
+        const openedPopup = document.querySelector('.popup_is-opened');
+        closePopup(openedPopup);
+    });
+})
+
+const popups = document.querySelectorAll('.popup');
+
+popups.forEach((popup) => {
+    popup.addEventListener('click', (evt) => {
+        if (evt.target === popup) {
+            closePopup(popup);
+        }
+    })
+})
+
