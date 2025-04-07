4abfe700f02ae717f40acebad676d222e1f83864
Leisan <Leisan>
2025-04-07 23:30:43 +1000

    Adding card functionality is added

--- src/index.js
+++ src/index.js
@@ -56,7 +56,11 @@ function handleEsc(evt) {
 const editButton = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup_type_edit');
 
-editButton.addEventListener('click', () => openPopup(popupEdit));
+editButton.addEventListener('click', () => {
+    openPopup(popupEdit);
+    document.forms["edit-profile"].elements.name.value = document.querySelector('.profile__title').textContent;
+    document.forms["edit-profile"].elements.description.value = document.querySelector('.profile__description').textContent;
+});
 
 const addButton = document.querySelector('.profile__add-button');
 const popupAdd = document.querySelector('.popup_type_new-card');
@@ -82,3 +86,66 @@ popups.forEach((popup) => {
     })
 })
 
+// Находим форму в DOM
+const formElement = popupEdit.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = formElement.querySelector('.popup__input_type_name'); // Воспользуйтесь инструментом .querySelector()
+const jobInput = formElement.querySelector('.popup__input_type_description'); // Воспользуйтесь инструментом .querySelector()
+
+
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    const name = nameInput.value;
+    const job = jobInput.value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+    const profileName = document.querySelector('.profile__title');
+    const profileJob = document.querySelector('.profile__description');
+
+    // Вставьте новые значения с помощью textContent
+    profileName.textContent = name;
+    profileJob.textContent = job;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
+
+
+
+// Находим форму в DOM
+const addImageFormElement = popupAdd.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const placeName = addImageFormElement.querySelector('.popup__input_type_card-name'); 
+const link = addImageFormElement.querySelector('.popup__input_type_url'); 
+
+
+function handleImageCreationSubmit(evt) {
+    evt.preventDefault(); 
+
+    const placeNameValue = placeName.value;
+    const linkValue = link.value
+
+    const newCardData = {
+        name: placeNameValue,
+        link: linkValue
+    }
+
+    const newCard = createCard(newCardData, removeCard);
+
+    cardsList.prepend(newCard);
+
+    placeName.value = "";
+    link.value = "";
+
+    closePopup(popupAdd);
+}
+
+addImageFormElement.addEventListener('submit', handleImageCreationSubmit);
